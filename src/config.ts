b63e8e5bbc17bce4903e8bab3d66524064ecaa16
NicholasC2 <137032388+NicholasC2@users.noreply.github.com>
2026-08-08 09:43:54 +1200

    Add vid and pid to default config

--- src/config.ts
+++ src/config.ts
@@ -3,6 +3,8 @@ import { defineConfig } from "./types";
 export default [
     defineConfig({
         id: "MP001",
+        VID: 0xF1F1, // winry/winry315 default
+        PID: 0x0315, // winry/winry315 default
     
         persistentKeys: new Map([
             [
